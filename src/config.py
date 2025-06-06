2c937b7673d4070ccd350c0af2718fe089408542
Felipe Cordeiro Ferreira <felicordeirofcf@gmail.com>
2025-06-06 17:48:49 -0300

    Sua mensagem de commit aqui

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 
 # Configurações do banco de dados
-DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///./test.db')
+DATABASE_URL = 'postgresql://postgres:WOBJFhAuvKGKURZOInheKDQWPQwYDgxz@postgres.railway.internal:5432/railway'
 
 # Configurações do Google OAuth
 GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID')
@@ -9,6 +9,6 @@ GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET')
 GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI')
 
 # Chave secreta para sessões do Flask
-SECRET_KEY = os.environ.get('SECRET_KEY', 'uma_chave_secreta_muito_segura')
+SECRET_KEY = 'bc132d7da14069340f332a6d814071b57cee8b9a3322a516'
 
 
